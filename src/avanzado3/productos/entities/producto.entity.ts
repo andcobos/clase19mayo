2eddc104f75f4310e64eeb26da2cf0bfca7423da
renderos1-1 <20235067@esen.edu.sv>
2025-05-23 23:25:36 -0600

    ejercico avanzado 3

---
+++ src/avanzado3/productos/entities/producto.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Comentario } from '../../avanzado3/comentarios/entities/comentario.entity';
+
+@Entity('productos')
+export class Producto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  nombre: string;
+
+  @Column({ type: 'text', nullable: true })
+  descripcion: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  precio: number;
+
+  @OneToMany(() => Comentario, comentario => comentario.producto)
+  comentarios: Comentario[];
+}
